094d6866058498e93f72b9379c1e23bf31205ff6
memcmahon <mcmahon.meganelizabeth@gmail.com>
2019-10-24 11:16:19 -0600

    Megans Walkthrough

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,12 +1,89 @@
 class Museum
-  attr_reader :name, :exhibits
+  attr_reader :name,
+              :patrons,
+              :patrons_of_exhibits,
+              :revenue
 
   def initialize(name)
     @name = name
-    @exhibits = []
+    # @exhibits = []
+    @patrons = []
+    @patrons_of_exhibits = {}
+    @revenue = 0
+  end
+
+  def exhibits
+    @patrons_of_exhibits.keys
   end
 
   def add_exhibit(exhibit)
-    @exhibits << exhibit
+    # @exhibits << exhibit
+    @patrons_of_exhibits[exhibit] = []
+  end
+
+  def admit(patron)
+    @patrons << patron
+    exhibits_by_cost.each do |exhibit|
+      if patron.spending_money >= exhibit.cost && patron.interests.include?(exhibit.name)
+        @patrons_of_exhibits[exhibit] << patron
+        patron.spending_money -= exhibit.cost
+        @revenue += exhibit.cost
+      end
+    end
+  end
+
+  def recommend_exhibits(patron)
+    # need exhibit name, individually
+    # does interests include the exhibit.name? If yes, recommend
+    # recommended = []
+    # @exhibits.each do |exhibit|
+    #   if patron.interests.include?(exhibit.name)
+    #     recommended << exhibit
+    #   end
+    # end
+    # recommended
+
+    exhibits.find_all do |exhibit|
+      patron.interests.include?(exhibit.name)
+    end
+
+    # recommended = [] << This causes double iteration, so be careful!!
+    # patron.interests.each do |interest|
+    #   recommended << @exhibits.find { |exhibit| exhibit.name == interest }
+    # end
+    # recommended
+  end
+
+  def patrons_by_exhibit_interest
+    # return a hash; keys: exhibits, values: array of patrons interested in that exhibit
+    ##################
+    # patrons_by_exhibit_interest = {}
+    # @exhibits.each do |exhibit|
+    #   interested_patrons = @patrons.find_all do |patron|
+    #     patron.interests.include?(exhibit.name)
+    #   end
+    #   patrons_by_exhibit_interest[exhibit] = interested_patrons
+    #   # for each exhibit, iterate over patrons and find the ones interested in exh
+    # end
+    # patrons_by_exhibit_interest
+    ###################
+    patrons_by_exhibit_interest = {}
+    exhibits.each do |exhibit|
+      patrons_by_exhibit_interest[exhibit] = interested_patrons(exhibit)
+    end
+    patrons_by_exhibit_interest
+  end
+
+  #HELPER METHODS
+  def interested_patrons(exhibit)
+    @patrons.find_all do |patron|
+      patron.interests.include?(exhibit.name)
+    end
+  end
+
+  def exhibits_by_cost
+    exhibits.sort_by do |exhibit|
+      exhibit.cost
+    end.reverse
   end
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -17,6 +17,7 @@ class MuseumTest < Minitest::Test
 
     assert_equal "Denver Museum of Nature and Science", dmns.name
     assert_equal [], dmns.exhibits
+    assert_equal [], dmns.patrons
   end
 
   def test_it_adds_exhibits
@@ -30,5 +31,174 @@ class MuseumTest < Minitest::Test
     dmns.add_exhibit(imax)
 
     assert_equal [gems_and_minerals, dead_sea_scrolls, imax], dmns.exhibits
+  end
+
+  def test_it_admits_patrons
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    bob = Patron.new("Bob", 20)
+    sally = Patron.new("Sally", 20)
+
+    dmns.admit(bob)
+    dmns.admit(sally)
+
+    assert_equal [bob, sally], dmns.patrons
+  end
+
+  def test_it_recommends_exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    imax = Exhibit.new("IMAX", 15)
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    bob = Patron.new("Bob", 20)
+    sally = Patron.new("Sally", 20)
+    bob.add_interest("Dead Sea Scrolls")
+    bob.add_interest("Gems and Minerals")
+    sally.add_interest("IMAX")
+
+    assert_equal 2, dmns.recommend_exhibits(bob).length
+    assert_equal true, dmns.recommend_exhibits(bob).include?(gems_and_minerals)
+    assert_equal true, dmns.recommend_exhibits(bob).include?(dead_sea_scrolls)
+    # assert_equal [gems_and_minerals, dead_sea_scrolls], dmns.recommend_exhibits(bob)
+    assert_equal [imax], dmns.recommend_exhibits(sally)
+  end
+
+  def test_it_returns_patrons_by_exh_interest
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    imax = Exhibit.new("IMAX", 15)
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    bob = Patron.new("Bob", 20)
+    bob.add_interest("Dead Sea Scrolls")
+    bob.add_interest("Gems and Minerals")
+    sally = Patron.new("Sally", 20)
+    sally.add_interest("Dead Sea Scrolls")
+    dmns.admit(bob)
+    dmns.admit(sally)
+
+    expected = {
+      gems_and_minerals => [bob],
+      dead_sea_scrolls => [bob, sally],
+      imax => [],
+    }
+
+    assert_equal expected, dmns.patrons_by_exhibit_interest
+    # assert_equal [], dmns.patrons_by_exhibit_interest[imax]
+  end
+
+  def test_it_returns_patrons_of_exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    imax = Exhibit.new("IMAX", 15)
+    dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(imax)
+    dmns.add_exhibit(dead_sea_scrolls)
+
+    # This Patron is interested in two exhibits but none in their price range, so they attend none
+    tj = Patron.new("TJ", 7)
+    tj.add_interest("IMAX")
+    tj.add_interest("Dead Sea Scrolls")
+    dmns.admit(tj)
+
+    # This Patron is interested in two exhibits and only Dead Sea Scrolls
+    # is in their price range price, so they attend Dead Sea Scrolls
+    bob = Patron.new("Bob", 10)
+    bob.add_interest("Dead Sea Scrolls")
+    bob.add_interest("IMAX")
+    dmns.admit(bob)
+
+    # This Patron is interested in two exhibits and both are in their price range.
+    # They attend the more expensive one first (IMAX), but don't have enough money to attend
+    # the second one
+    sally = Patron.new("Sally", 20)
+    sally.add_interest("IMAX")
+    sally.add_interest("Dead Sea Scrolls")
+    dmns.admit(sally)
+
+    # This Patron is interested in two exhibits and both are in their price range.
+    # They have enough spending money to afford both, so they attend both.
+    morgan = Patron.new("Morgan", 15)
+    morgan.add_interest("Gems and Minerals")
+    morgan.add_interest("Dead Sea Scrolls")
+    dmns.admit(morgan)
+
+    expected = {
+      gems_and_minerals => [morgan],
+      dead_sea_scrolls => [bob, morgan],
+      imax => [sally],
+    }
+
+    assert_equal expected, dmns.patrons_of_exhibits
+  end
+
+  def test_it_has_revenue
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    imax = Exhibit.new("IMAX", 15)
+    dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(imax)
+    dmns.add_exhibit(dead_sea_scrolls)
+
+    assert_equal 0, dmns.revenue
+
+    tj = Patron.new("TJ", 7)
+    tj.add_interest("IMAX")
+    tj.add_interest("Dead Sea Scrolls")
+    dmns.admit(tj)
+
+    bob = Patron.new("Bob", 10)
+    bob.add_interest("Dead Sea Scrolls")
+    bob.add_interest("IMAX")
+    dmns.admit(bob)
+
+    sally = Patron.new("Sally", 20)
+    sally.add_interest("IMAX")
+    sally.add_interest("Dead Sea Scrolls")
+    dmns.admit(sally)
+
+    morgan = Patron.new("Morgan", 15)
+    morgan.add_interest("Gems and Minerals")
+    morgan.add_interest("Dead Sea Scrolls")
+    dmns.admit(morgan)
+
+    assert_equal 35, dmns.revenue
+  end
+
+  def test_admitting_patrons_reduces_spending_money
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    imax = Exhibit.new("IMAX", 15)
+    dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(imax)
+    dmns.add_exhibit(dead_sea_scrolls)
+    tj = Patron.new("TJ", 7)
+    tj.add_interest("IMAX")
+    tj.add_interest("Dead Sea Scrolls")
+    dmns.admit(tj)
+    bob = Patron.new("Bob", 10)
+    bob.add_interest("Dead Sea Scrolls")
+    bob.add_interest("IMAX")
+    dmns.admit(bob)
+    sally = Patron.new("Sally", 20)
+    sally.add_interest("IMAX")
+    sally.add_interest("Dead Sea Scrolls")
+    dmns.admit(sally)
+    morgan = Patron.new("Morgan", 15)
+    morgan.add_interest("Gems and Minerals")
+    morgan.add_interest("Dead Sea Scrolls")
+    dmns.admit(morgan)
+
+    assert_equal 7, tj.spending_money
+    assert_equal 0, bob.spending_money
+    assert_equal 5, sally.spending_money
+    assert_equal 5, morgan.spending_money
   end
 end
